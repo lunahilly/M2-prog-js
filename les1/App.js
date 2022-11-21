695a572385c8578ab9ee2cb8fb47f7b1f872b854
Luna <luna.software.development@gmail.com>
2022-11-21 10:15:17 +0100

    les 1

---
+++ les1/App.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!");
+        //code hier
+        let appNaam = "fuck";
+        let versienummer = 1;
+        let versiedatum = "21/11/2022";
+        let autheur = "Luna";
+        let copyright = "Hilversum";
+        let distributeur = "Luna Hilversum";
+        let darkmode = false;
+        console.log(appNaam);
+        console.log(versiedatum);
+        console.log(versienummer);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+}
+let app = new App();
+app.runApplication(); 
